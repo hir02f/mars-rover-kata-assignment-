a6e9888e42a23471f2ed90dbd868fb322b3e27d4
Jas <doggrell@gmail.com>
2022-09-20 19:11:47 +0100

    Console can take Plateau coordinates in correct format

--- MarsRover/Program.cs
+++ MarsRover/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MarsRover;
+
+// Plateau input
+Console.WriteLine("Please enter upper-right coordinates of the Plateau");
+string plateauInput = Console.ReadLine();
+string[] plateauInputArray = plateauInput.Split(' ');
+List<int> plateauCoordinates = new List<int> {};
+
+try
+{
+    bool inputOK = new UserInterface().checkInputForPlateau(plateauInputArray, plateauCoordinates);
+}
+catch (System.ArgumentException e)
+{
+    Console.WriteLine(e.Message);
+    return;
+}
---
+++ MarsRover/UserInterface.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover
+{
+    public class UserInterface
+    {
+        private const string VALID_ORIENTATION = "N|E|S|W";
+        private const int ZERO = 0; 
+
+        public bool checkInputForPlateau(string[] plateauInputArray, List<int> plateauCoordinates)
+        {
+            foreach (var coord in plateauInputArray)
+            {
+                try
+                {
+                    plateauCoordinates.Add(int.Parse(coord));
+                }
+                catch (System.FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return false;
+                }
+            }
+
+            if (plateauCoordinates.Count > 2)
+            {
+                Console.WriteLine("Just enter two numbers followed by a space in between them!");
+                return false;
+            }
+
+            if (plateauCoordinates[0] < ZERO || plateauCoordinates[1] < ZERO)
+            {
+                throw new ArgumentException("Position must be more than zero!");
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool checkInputForRover(int x, int y, char o)
+        {
+            if (x < ZERO || y < ZERO)
+            {
+                throw new ArgumentException("Position must be more than zero!");
+            }
+            else if (!VALID_ORIENTATION.Contains(o.ToString()))
+            {
+                throw new ArgumentException("Orentation must be N, E, S or W!");
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}

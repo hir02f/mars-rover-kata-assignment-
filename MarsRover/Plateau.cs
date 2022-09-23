2f751abdc7bc9e7b6b731386239e9c6790bdf3f9
Jas <doggrell@gmail.com>
2022-09-23 22:19:10 +0100

    Removed redundant grid of rovers

--- MarsRover/Plateau.cs
+++ MarsRover/Plateau.cs
@@ -5,8 +5,6 @@
         public int MaxX { get; private set; }
         public int MaxY { get; private set; }
 
-        public List<int[]> GridOfRovers = new List<int[]>();
-
         public Plateau(int x, int y)
         {
             MaxX = x;
